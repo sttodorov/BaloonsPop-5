3ea06aaaa0b65d1a2b2d1b8120744d50a46cc816
TodorVyagov <Todor@todor.todor>
2014-07-01 15:21:27 -0700

    PopEngine added validations. PopEngine PopAt() all tests added. Issue #15 closed.

--- BaloonsPopGame.Tests/PopEngineTests.cs
+++ BaloonsPopGame.Tests/PopEngineTests.cs
@@ -19,7 +19,7 @@
                 { 8, 1, 2, 2, 3, 4, 2, 3, 4, 1, 2 },
                 { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
                 { 8, 5, 2, 2, 3, 4, 2, 3, 4, 5, 2 },
-                { 1, 1, 2, 2, 4, 4, 4, 6, 4, 1, 2 },
+                { 1, 1, 2, 2, 4, 4, 4, 6, 4, 1, 3 },
                 { 2, 1, 4, 1, 3, 4, 2, 3, 4, 1, 2 },
                 { 2, 3, 7, 3, 3, 4, 2, 3, 1, 3, 2 },
                 { 8, 1, 2, 2, 3, 4, 2, 3, 4, 2, 2 }
@@ -46,6 +46,28 @@
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PopAtNullFieldTest()
+        {
+            PopEngine.PopAt(3, 4, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void PopAtInvalidPositionTest()
+        {
+            PopEngine.PopAt(10, 10, actualField);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PopAtEmptyCell()
+        {
+            actualField[4, 6] = 0;
+            PopEngine.PopAt(4, 6, actualField);
+        }
+
+        [TestMethod]
         public void PopAtOneBaloonTest()
         {
             PopEngine.PopAt(2, 2, actualField);
@@ -56,10 +78,61 @@
         }
 
         [TestMethod]
-        public void PopAtOneWholeRowtest()
+        public void PopAtWholeRowTest()
         {
-            PopEngine.PopAt();
+            PopEngine.PopAt(4, 0, actualField);
+            for (int col = 0; col < actualField.NumberOfColumns; col++)
+            {
+                expectedField[4, col] = 0;
+            }
+            bool areEqual = CompareFields();
 
+            Assert.IsTrue(areEqual);
+        }
+
+        [TestMethod]
+        public void PopAtWholeColTest()
+        {
+            PopEngine.PopAt(9, 5, actualField);
+            for (int row = 0; row < actualField.NumberOfRows; row++)
+            {
+                expectedField[row, 5] = 0;
+            }
+            bool areEqual = CompareFields();
+
+            Assert.IsTrue(areEqual);
+        }
+
+        [TestMethod]
+        public void PopAtWholeRowAndColTest()
+        {
+            PopEngine.PopAt(4, 5, actualField);
+            for (int col = 0; col < actualField.NumberOfColumns; col++)
+            {
+                expectedField[4, col] = 0;
+            }
+
+            for (int row = 0; row < actualField.NumberOfRows; row++)
+            {
+                expectedField[row, 5] = 0;
+            }
+            bool areEqual = CompareFields();
+
+            Assert.IsTrue(areEqual);
+        }
+
+        [TestMethod]
+        public void PopAtCornerTest()
+        {
+            PopEngine.PopAt(9, 10, actualField);
+            expectedField[9, 9] = 0;
+            expectedField[9, 10] = 0;
+            expectedField[8, 10] = 0;
+            expectedField[7, 10] = 0;
+
+            bool areEqual = CompareFields();
+
+            Assert.IsTrue(areEqual);
         }
     }
 }
