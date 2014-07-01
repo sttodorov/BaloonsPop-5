21e7fb0513a09618388339c367dbc65a3364b03f
krumov <krasin.krumov@gmail.com>
2014-07-01 13:45:40 +0300

    Added 1 unit test for TopFive();
    Also added 3 test files

---
+++ BaloonsPopGame.Tests/RankListStorageTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace BaloonsPopGame.Tests
+{
+    [TestClass]
+    public class RankListStorageTests
+    {
+        [TestMethod]
+        public void TopFiveNullTest()
+        {
+            string filePath = @"..\..\TestFiles\ranklist.txt";
+            IStorage storage = new RankListStorage(filePath);
+            
+            var resultTopFive = storage.TopFive();
+
+            var expectedCount = 0;
+            var resultCount = resultTopFive.Count;
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+
+        [TestMethod]
+        public void TopFiveNormalTest()
+        {
+            string filePath = @"..\..\TestFiles\ranklist1.txt";
+            IStorage storage = new RankListStorage(filePath);
+            var topFive = new List<RankListRecord>(5);
+
+            var resultTopFive = storage.TopFive();
+
+            var expectedCount = 0;
+            var resultCount = resultTopFive.Count;
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+    }
+}
