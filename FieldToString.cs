d6af8ea74cb30993184fba7ba4151c4c9b3e2d8b
krumov <krasin.krumov@gmail.com>
2014-07-15 17:28:58 +0300

    Added DrawEmptyFrame method to FieldToStrings.cs

--- FieldToString.cs
+++ FieldToString.cs
@@ -55,5 +55,51 @@
             var fieldString = builder.ToString();
             return fieldString;
         }
+
+        public static string DrawEmptyFrame(byte[,] fieldClone)
+        {
+            if (fieldClone == null)
+            {
+                throw new ArgumentNullException("Field cannot be null!");
+            }
+
+            var builder = new StringBuilder();
+            int fieldColumns = fieldClone.GetLength(1);
+
+            builder.Append("    ");
+            // Print Column numbers
+            for (byte column = 0; column < fieldColumns; column++)
+            {
+                builder.Append(string.Format("{0} ", column));
+            }
+
+            builder.AppendLine();
+            builder.Append("   ");
+
+            // Print dashes between baloons and indexes
+            builder.Append(new string('-', fieldColumns * 2 + 1));
+            builder.AppendLine();
+
+            for (byte row = 0; row < fieldClone.GetLength(0); row++)
+            {
+                // Print number of Row
+                builder.Append(row + " | ");
+                for (byte col = 0; col < fieldColumns; col++)
+                {
+                    builder.Append("  ");                     
+                }
+
+                builder.Append("|");
+                builder.AppendLine();
+            }
+
+            builder.Append("   ");
+            builder.Append(new string('-', fieldColumns * 2 + 1));
+            builder.AppendLine();
+
+            var fieldString = builder.ToString();
+            return fieldString;
+        }
+
     }
 }
