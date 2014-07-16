525178abc3b8c1071baa732e191f7e5aadd2e0f3
sttodorov <st.todorov9@gmail.com>
2014-07-16 19:14:51 +0300

    Change GameFieldFacede access modifier

--- BaloonsPopGame.Tests/GameFieldFacadeTests.cs
+++ BaloonsPopGame.Tests/GameFieldFacadeTests.cs
@@ -4,7 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    class GameFieldFacadeTests
+    public class GameFieldFacadeTests
     {
         private GameFieldFacade facade;
 
