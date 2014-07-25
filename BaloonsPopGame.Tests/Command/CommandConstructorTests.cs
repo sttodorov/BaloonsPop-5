25d9fa7966894b337642f91071d5a1ef6e117259
dev705 <dev705@users.noreply.github.com>
2014-07-25 21:17:41 +0300

    Found missing tests file

---
+++ BaloonsPopGame.Tests/Command/CommandConstructorTests.cs
@@ -0,0 +1,52 @@
+﻿namespace BaloonsPopGame.Engine.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CommandConstructorTests
+    {
+        [TestMethod]
+        public void CommandExitIsCorrectType()
+        {
+            var testCommand = new Command(CommandType.Exit);
+            var expectedType = CommandType.Exit;
+
+            Assert.AreEqual(expectedType, testCommand.Type);
+        }
+
+        [TestMethod]
+        public void CommandRestartIsCorrectType()
+        {
+            var testCommand = new Command(CommandType.Restart);
+            var expectedType = CommandType.Restart;
+
+            Assert.AreEqual(expectedType, testCommand.Type);
+        }
+
+        [TestMethod]
+        public void CommandPassesCoordinateDataCorrectly()
+        {
+
+            var expectedCoord = new int[2] { 2, 7 };
+            var testCommand = new Command(CommandType.PopBalloonAt, expectedCoord);
+            var actualCoord = (int[])testCommand.Data;
+
+            bool sameCoord = expectedCoord[0] == actualCoord[0] &&
+                expectedCoord[0] == actualCoord[0];
+
+            Assert.IsTrue(sameCoord);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CommandCallingDataPropertyIfNotTypePopBalloonAt_throwsException()
+        {
+            var testCommand = new Command(CommandType.Restart);
+            var data = testCommand.Data;
+         
+        }
+
+    }
+}
