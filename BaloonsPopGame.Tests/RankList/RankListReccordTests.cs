b5efec33c1ae1148370692a473630557cc6ec1c1
TodorVyagov <rambo007@abv.bg>
2014-07-25 23:47:40 +0300

    Added some Unit tests for RankListRecord(now tested on 100%) много неадекватно го написах...

--- BaloonsPopGame.Tests/RankList/RankListReccordTests.cs
+++ BaloonsPopGame.Tests/RankList/RankListReccordTests.cs
@@ -7,6 +7,20 @@
     public class RankListReccordTests
     {
         [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RankListRecordCreateWithInvalidValue()
+        {
+            var record = new RankListRecord(0, "pesho");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RankListRecordCreateWithInvalidName()
+        {
+            var record = new RankListRecord(6, " ");
+        }
+
+        [TestMethod]
         public void RankListReccordToString()
         {
             var reccord = new RankListRecord(15, "A happy name");
@@ -28,6 +42,26 @@
             var expected = ".    A happy name                   -  15 moves.";
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CompareToHigherResult()
+        {
+            var recordWithHigherScore = new RankListRecord(10, "pesho");
+            var recordWithLowerScore = new RankListRecord(3, "gosho");
+            int result = recordWithHigherScore.CompareTo(recordWithLowerScore);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void CompareToEqualResult()
+        {
+            var record1 = new RankListRecord(10, "pesho");
+            var record2 = new RankListRecord(10, "gosho");
+            int result = record1.CompareTo(record2);
+
+            Assert.AreEqual(result, 0);
         }
     }
 }
